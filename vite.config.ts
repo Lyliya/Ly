cf138232dde3cbd3cec900a82ef75f6e09eaeacb
Guillaume Guerin <guerin.dev@gmail.com>
2024-09-05 22:33:18 +0200

    chore: dynamic chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
-import { resolve } from "node:path";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
+import { resolve } from "path";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dts()],
   build: {
+    cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "Ly",
-      filename: "ly",
+      fileName: "ly",
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["vue"],
       output: {
+        inlineDynamicImports: false,
+        chunkFileNames: "chunks/[name].[hash].js",
+        assetFileNames: "assets/[name][extname]",
+        preserveModules: true,
+        entryFileNames: "[name].js",
         globals: {
           vue: "Vue",
         },
+        exports: "named",
       },
     },
   },
