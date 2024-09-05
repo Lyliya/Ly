7db7e2748f5d1cf1f882369ec83653f1a07f49ce
Guillaume Guerin <guerin.dev@gmail.com>
2024-09-05 22:33:53 +0200

    fix: global wrapper for histoire

--- src/histoire/GlobalWrapper.vue
+++ src/histoire/GlobalWrapper.vue
@@ -1,21 +1,7 @@
-<script lang="ts" setup>
-import { Story, Variant } from "histoire";
-
-const props = defineProps<{
-  story: Story;
-  variant?: Variant;
-}>();
-
-function hasWrapper() {
-  return (
-    props.story.meta?.wrapper !== false &&
-    props.variant?.meta?.wrapper !== false
-  );
-}
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="global-wrapper" :style="hasWrapper() ? 'padding: 0.5rem;' : ''">
+  <div class="global-wrapper" :style="'padding: 0.5rem;'">
     <slot />
   </div>
 </template>
