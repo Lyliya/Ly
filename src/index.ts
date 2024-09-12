59975cd9bca635eefbf1392e07f8c4388f0998ea
Guillaume Guerin <guerin.dev@gmail.com>
2024-09-12 13:02:49 +0200

    feat: export LyLoader

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import "@/style.scss";
 
 import LyButton from "@/components/LyButton/LyButton.vue";
 import LyBadge from "@/components/LyBadge/LyBadge.vue";
+import LyLoader from "@/components/LyLoader/LyLoader.vue";
 
-export { LyButton, LyBadge };
+export { LyLoader, LyButton, LyBadge };
