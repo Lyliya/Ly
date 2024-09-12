58f1c79b080c56b3a04813a62f8821e58da78451
Guillaume Guerin <guerin.dev@gmail.com>
2024-09-12 14:58:04 +0200

    fix: change how css is bundled

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts(), cssInjectedByJsPlugin()],
   build: {
-    cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "Ly",
